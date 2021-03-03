7c788405c1d73b5357ceea78b3d76668790a1a42
EC2 Default User <ec2-user@ip-172-31-7-51.ec2.internal>
2021-03-03 01:36:02 +0000

    Finished part 3 using original code.

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -19,7 +19,10 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  # Note: character '.' is not equivalent to '\s\S' because '.' do not 
+  # catch line terminators
+  expect(/#{e1}[\s\S]*#{e2}/).to match(page.body)
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -30,9 +33,9 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  ratings = rating_list.split(', ')
-  ratings.each do |rating|
-    uncheck ? uncheck("ratings[#{rating}]") : (check("ratings[#{rating}]"))
+  rating_split = rating_list.split(', ')
+  rating_split.each do |elem|
+    uncheck ? uncheck("ratings[#{elem}]") : (check("ratings[#{elem}]"))
   end
   #fail "Unimplemented"
 end
